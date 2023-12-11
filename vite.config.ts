9970592d6af62f1e45361c78316bcd73cef18d13
辻 <tsuji.kento@njc.co.jp>
2023-12-11 11:45:15 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import {  VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+    ? "reactailwind"              // この行を追加
+    : "./",                       // この行を追加
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'PWA Sample Apps',
+        short_name: 'PWAApps',            // ホーム画面に追加した時に表示される名前
+        description: 'PWAサンプルアプリ',
+        theme_color: '#ffffff',
+        // 各サイズごとのアイコンは下記で生成する。
+        // https://www.pwabuilder.com/imageGenerator
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
+})
