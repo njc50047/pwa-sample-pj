178caf7b42220a65b7b9e5dc9c50e3f769e6b1aa
辻 <tsuji.kento@njc.co.jp>
2023-12-21 18:56:29 +0900

    設定部分の修正

--- src/OneSignalInitial.tsx
+++ src/OneSignalInitial.tsx
@@ -6,8 +6,8 @@ export const OneSignalInitial = () => {
     useEffect(() => {
         const oneSignalInit = async () => {
           await OneSignal.init({
-            appId: '48acf5cf-a77b-4c97-ab6f-e2d69f19efe1',
-            safari_web_id: "web.onesignal.auto.0908e376-c893-48cd-92f7-007572df5c49",
+            appId: 'ここにOneSignalで発行されたIDを設定',
+            safari_web_id: "ここにOneSignalで発行されたIDを設定",
             allowLocalhostAsSecureOrigin: true,
           }).then(() => {
             OneSignal.Slidedown.promptPush()
